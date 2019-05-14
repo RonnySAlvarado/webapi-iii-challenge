63b4aaeec3f06464dee295ca25c64df304e1d551
unknown <Rsalvarado777@gmail.com>
2019-05-14 18:56:50 -0500

    Set up the boiler plate for listening to server and importing all required dependencies

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const express = 'express';
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const server = express();
 
@@ -6,10 +8,16 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
-//Initial commit
-function logger(req, res, next) {
+//thirdpartymiddleware because why tf not amirite
+server.use(cors());
+server.use(morgan('dev'));
+server.use(helmet());
 
+//custom middleware
+server.use(logger);
+
+function logger(req, res, next) {
+  console.log("Request type:", )
 };
 
 module.exports = server;
